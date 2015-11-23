6d5f928cbad51aea39d20fd5bb7952f2c1718bf9
qianfeng <qianfeng@hahayixiaoWIFI.local>
2015-11-23 14:53:31 +0800

    verson1

---
+++ DemoSuccess/DemoSuccess/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DemoSuccess
+//
+//  Created by qianfeng on 15/11/23.
+//  Copyright (c) 2015年 DBBPerson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
